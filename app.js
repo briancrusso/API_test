33d2d74acf6e6ab77ad52c83533e23d64252368c
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-06-12 15:41:43 -0400

    API working

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
+
 $(function() {
     $('#search-term').submit(function(event) {
         event.preventDefault();
-        var searchTerm = $('#query').val();
+         var searchTerm = $('#query').val();
        getRequest(searchTerm);
+       console.log(searchTerm);
     });
 });
 
 function getRequest(searchTerm){
 	var params = {
-		part: 'snippet',
-		key: 'AIzaSyBe88cebijSWuTD3ocPRSxXdb25n8FHbcE',
-//Am I defining 'searchTerm' as a string correctly?
+	part: 'snippet',
+	key: 'AIzaSyA8f5SfIBo7Lnn6Z-ouW3kw-BbXn6Z6EPc',
 		q: searchTerm,
 		r: 'json'
 	};
 	url = 'https://www.googleapis.com/youtube/v3/search';
 
 	$.getJSON(url, params, function(data){
-		showResults(data.Search);
+		showResults(data.items[0].snippet.thumbnails);
+        console.log(data.items[0].snippet.thumbnails);
 	});
 }
 
 function showResults(results) {
     var html = "";
     $.each(results, function(index, value) {
-        html += '<p>' + value.Title + value.Year + '</p>'
-        console.log(value.Title);
+        html += '<li>' + '<a href="' + value.url + '">' + value.url + '</a>' + '</li>';
+        console.log(value.url);
     });
     $('#search-results').html(html);
 }
